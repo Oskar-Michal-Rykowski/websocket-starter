dc9fc9e3164af059d2f236891e0de798099953cb
Oskar Rykowski <oskar.michal.rykowski@gmail.com>
2021-12-12 14:32:16 +0100

    Create chat and implement websocket connection

--- client/app.js
+++ client/app.js
@@ -1,3 +1,7 @@
+const socket = io();
+
+socket.on('message', ({ author, content }) => addMessage(author, content));
+
 const loginForm = document.getElementById('welcome-form');
 const messagesSection = document.getElementById('messages-section');
 const messagesList = document.getElementById('messages-list');
@@ -24,25 +28,26 @@ function addMessage(author, content) {
   message.classList.add('message--received');
   if (author === userName) {
     message.classList.add('message--self');
-    message.innerHTML = `
-            <h3 class="message__author">${
-              userName === author ? 'You' : author
-            }</h3>
-            <div class="message__content">
-                ${content}
-            </div>
-        `;
-    messagesList.appendChild(message);
   }
+  message.innerHTML = `<h3 class="message__author">${
+    userName === author ? 'You' : author
+  }</h3><div class="message__content">${content}</div>`;
+  messagesList.appendChild(message);
 }
 
-const sendMessage = function (event) {
-  event.preventDefault();
-  if (messageContentInput.value) {
-    addMessage(userName, messageContentInput.value);
+function sendMessage(e) {
+  e.preventDefault();
+
+  let messageContent = messageContentInput.value;
+
+  if (!messageContent.length) {
+    alert('You have to type something!');
+  } else {
+    addMessage(userName, messageContent);
+    socket.emit('message', { author: userName, content: messageContent });
     messageContentInput.value = '';
   }
-};
+}
 
 loginForm.addEventListener('submit', login);
 addMessageForm.addEventListener('submit', sendMessage);
